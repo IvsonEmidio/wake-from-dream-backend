84b7cfea41251239f08b7a20e10fd0173e365e19
IvsonEmidio <80026873+IvsonEmidio@users.noreply.github.com>
2022-04-10 22:23:31 -0300

    Moved middlewares to separated files

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,12 @@
-import express,  { Application } from "express";
+import express, { Application } from "express";
 import reportsRoutes from "./Routes/reportsRoutes";
-import helmet from "helmet";
-import cors from "cors";
-import rateLimit from "express-rate-limit";
+import publicRoutesMiddleware from "./Middlewares/publicRoutes";
 
 const app: Application = express();
 const port: number = 3000;
-const rateLimitConfig = {
-    windowMs: 15 * 60 * 1000,
-    max: 100,
-    message: "Too many requests from this IP, please try again after a minute"
-}
 
 //Middlewares
-app.use(helmet());
-app.use(cors());
-app.use(rateLimit(rateLimitConfig));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+publicRoutesMiddleware(app);
 
 //Routes
 reportsRoutes(app);
