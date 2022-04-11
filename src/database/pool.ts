d866c0acd3b7fba309553f0b7e711f2ec29d8b9a
IvsonEmidio <80026873+IvsonEmidio@users.noreply.github.com>
2022-04-10 22:25:43 -0300

    Created new connection database method 'Pool'

---
+++ src/database/pool.ts
@@ -0,0 +1,12 @@
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'wake-from-dream',
+  password: '94670302',
+  port: 5432,
+});
+
+
+export default pool;
