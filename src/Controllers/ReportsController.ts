394a7283782cc73fb1ed579cac7717b151059a07
IvsonEmidio <80026873+IvsonEmidio@users.noreply.github.com>
2022-04-10 22:24:29 -0300

    Removed some unused methods

--- src/Controllers/ReportsController.ts
+++ src/Controllers/ReportsController.ts
@@ -1,65 +1,108 @@
-import Reports from "../Domain/Reports";
-import AmericanStrategy from "../Domain/Strategy/Reports/AmericanStrategy";
-import BrazilianStrategy from "../Domain/Strategy/Reports/BrazilianStrategy";
-import IReportStrategy from "../Domain/StrategyInterfaces/IReportsStrategy";
-import { IReportPostParameters } from "../Interfaces/IReports";
+import pool from "../Database/pool";
+import Reports from "../Domain/ReportsContext";
+import { IReportEventsObj, IReportPostParameters } from "../Interfaces/IReports";
+import { body, check, Result, ValidationChain, validationResult } from "express-validator";
+import { Request, Response } from "express";
+import ReportsService from "../Services/ReportsService";
 
 export default class ReportsController {
-    /**
-     * Get all reports by category name.
-     * @param {string} category 
-     * @returns {object}
-     */
-    public getReportsByCategory(category: string): object {
-        let strategy = this.getStrategyByCategory(category);
-        let reports = new Reports(strategy);
+    reportsService: ReportsService;
 
-        let response = reports.getReports();
-
-        if (response) {
-            return response;
-        } else {
-            return {
-                status: 0,
-                msg: 'generic error'
-            }
-        }
+    constructor() {
+        this.reportsService = new ReportsService();
     }
 
     /**
-     * Adds a new report on database.
-     * @param {IReportPostParameters} data  
-     * @returns {object}
+     * Adds a new single report on database.
+     * @param {Request} req
+     * @param {Response} req
      */
-    public addReport(data: IReportPostParameters): object {
-        let strategy = this.getStrategyByCategory(data.category);
-        let reports = new Reports(strategy);
+    public async createReport(req: Request, res: Response) {
+        try {
+            const errors = validationResult(req);
 
-        let response = reports.addReport(data);
-
-        if (response) {
-            return response;
-        } else {
-            return {
-                status: 0,
-                msg: 'generic error'
+            if (!errors.isEmpty()) {
+                return res.status(400).json(
+                    {
+                        status: 0,
+                        message: `Check the fields and try again`,
+                        errors: errors.array()
+                    });
             }
+            let data: IReportPostParameters = req.body;
+
+            try {
+                await pool.query('BEGIN');
+
+                //Insert new row on 'reports' table.
+                let reportQuery = `INSERT INTO reports (title, author_id, category_id, date) VALUES($1, $2, $3, $4) RETURNING id`;
+                let reportQueryValues = [data.title, data.author_id, data.category_id, data.date];
+                let insertReport = await pool.query(reportQuery, reportQueryValues);
+                const generatedReportId = insertReport.rows[0].id;
+
+                //Insert new row on 'reports_texts' table.
+                let reportTextQuery = `INSERT INTO reports_texts (report_id, full_text, final_things) VALUES($1, $2, $3)`;
+                let reportTextQueryValues = [generatedReportId, data.full_text, data.final_things];
+                await pool.query(reportTextQuery, reportTextQueryValues);
+
+                //Insert new row on 'reports_events' table
+                let events: IReportEventsObj = this.reportsService.parseEventsArrayToObject(data.events);
+                let eventQuery = `INSERT INTO reports_events (report_id, lights, out_of_body, seen_spirits, tunnel_vision, watched_life_movie, feel_peace_and_love, dont_want_come_back, no_more_death_fear, seen_death_parents, other_dimension, need_finish_mission) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`;
+                let eventQueryValues = [
+                    generatedReportId, events.light, events.out_of_body, events.seen_spirits,
+                    events.tunnel_vision, events.watched_life_movie, events.feel_peace_and_love,
+                    events.dont_want_come_back, events.no_more_death_fear, events.seen_death_parents,
+                    events.other_dimension, events.need_finish_mission];
+                await pool.query(eventQuery, eventQueryValues)
+
+                await pool.query('COMMIT');
+
+                return res.status(201).json({
+                    status: 1,
+                    message: 'Successfully created new report.',
+                    data: {
+                        id: generatedReportId
+                    }
+                });
+            } catch (err) {
+                await pool.query('ROLLBACK');
+                res.status(502).json({
+                    status: 0,
+                    message: 'Failed to insert new report on database, check the fields and try again.',
+                    errors: err,
+                })
+            }
+        } catch (err) {
+            res.status(500).json({
+                status: 0,
+                message: 'An unknown error has occurred when validating the fields.',
+                errors: err,
+            })
         }
     }
 
-    /**
-     * Get an instance of IReportsStrategy Interface..
-     * @param {string} category
-     * @returns {object}
-     */
-    private getStrategyByCategory(category: string): IReportStrategy {
-        switch (category) {
-            case 'American':
-                return new AmericanStrategy;
-            case 'Brazilian':
-                return new BrazilianStrategy;
+
+    public validateBody(method: string): Array<ValidationChain> {
+        const allowedEvents: Array<string> = [
+            'lights', 'out_of_body', 'seen_spirits', 'tunnel_vision', 'watched_life_movie',
+            'feel_peace_and_love', 'dont_want_come_back', 'no_more_death_fear', 'seen_death_parents',
+            'other_dimension', 'need_finish_mission'
+        ];
+
+        switch (method) {
+            case 'createReport':
+                return [
+                    body('title', `Invalid field 'title'.`).isString().isLength({ min: 10, max: 255 }),
+                    body('author_id', `Invalid field 'author_id'.`).optional().isInt(),
+                    body('category_id', `Invalid field 'category_id'.`).isInt(),
+                    body('date', `invalid field 'date'`).isDate(),
+                    body('full_text', `invalid field 'full_text'`).isString().isLength(({ min: 50 })),
+                    body('final_things', `invalid field 'final_things'`).optional().isString(),
+                    body('events', `invalid field 'events'`).optional().isArray({ min: 1 }),
+                    check('events.*', `Invalid event, check list of allowed events.`).isString().isIn(allowedEvents)
+                ];
             default:
-                return new BrazilianStrategy;
+                return [];
         }
     }
 }
